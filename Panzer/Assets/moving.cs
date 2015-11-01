d5a93b6ebbd6f6279ef68d9cc450be5cb5aaf096
KirillRedin <kirillredin.ua@gmail.com>
2015-11-01 14:43:07 +0200

    Map test

---
+++ Panzer/Assets/moving.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moving : MonoBehaviour {
+    public GameObject obj;
+    public Vector3 position;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F1))
+            transform.Translate(1, 0, 0);
+        position = obj.transform.position;
+	}
+}
